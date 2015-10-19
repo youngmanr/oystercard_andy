7de940022ef67b638f7b5eb06856dc4e106e1751
Andrew Dowell <adowellphoto@gmail.com>
2015-10-19 15:33:47 +0100

    completed challenge 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+  describe '#initialize' do
+    it 'has a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1) }
+    it 'updates the value of the balance' do
+      subject.top_up(5)
+      expect{subject.top_up(5)}.to change{ subject.balance }.by 5
+    end
+  end
+
 
 end
